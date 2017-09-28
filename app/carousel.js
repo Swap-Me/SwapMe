be65c899b0b5ffb78a977a6d2517012b8a13bacc
Donald Disha <dondished@gmail.com>
2017-09-28 16:23:35 -0400

    Some stuff

--- app/carousel.js
+++ app/carousel.js
@@ -1,31 +1,31 @@
-var observableModule = require("data/observable");
-var colorModule = require("color");
-var Color = colorModule.Color;
-var pageData = new observableModule.Observable();
-var myDataArray = [
-	{title:"Find cool and interesting items nearby", color: "#ffffff", image:"https://s3.amazonaws.com/swapmeimg/c1.png", height:"400px"},
-	{title:"Swipe Right to like something or Swipe Left to pass", color: "#ffffff", image:"https://s3.amazonaws.com/swapmeimg/c2.png"},
-	{title:"If they also Swipe Right, it's a Match!", color: "#ffffff", image:"https://s3.amazonaws.com/swapmeimg/c3.png"},
-	{title:"Only people you'vw matched with can message you", color: "#ffffff", image:"https://s3.amazonaws.com/swapmeimg/s-l503.jpg"},
-	{title:"Setup a time and place to Drop or Swap!", color: "#ffffff", image:"https://s3.amazonaws.com/swapmeimg/s-l505.jpg"}
-];
-pageData.set("myDataArray", myDataArray);
-var myCarousel = null;
+var observableModule = require('data/observable'),
+    // colorModule = require('color'),
+    // color = colorModule.Color,
+    // myCarousel = null,
+    pageData = new observableModule.Observable(),
+    myDataArray = [
+	    {title:'Find cool and interesting items nearby', color: '#ffffff', image:'https://s3.amazonaws.com/swapmeimg/c1.png', height:'400px'},
+	    {title:'Swipe Right to like something or Swipe Left to pass', color: '#ffffff', image:'https://s3.amazonaws.com/swapmeimg/c2.png'},
+	    {title:'If they also Swipe Right, it\'s a Match!', color: '#ffffff', image:'https://s3.amazonaws.com/swapmeimg/c4.png'},
+	    {title:'Setup a time and place to Drop or Swap!', color: '#ffffff', image:'https://s3.amazonaws.com/swapmeimg/c5.png'}
+    ];
+
+pageData.set('myDataArray', myDataArray);
 
 exports.pageLoaded = function(args) {
-    var page = args.object;
-    page.bindingContext = pageData;
-    myCarousel = page.getViewById("myCarousel");
-}
+  var page = args.object;
+  page.bindingContext = pageData;
+  myCarousel = page.getViewById('myCarousel');
+};
 
 // exports.myChangeEvent = function(args){
-//     var changeEventText = "Page changed to index: " + args.index;
-//     pageData.set("changeEventText", changeEventText);
+//     var changeEventText = 'Page changed to index: ' + args.index;
+//     pageData.set('changeEventText', changeEventText);
 // }
 
 // exports.mySelectedEvent = function(args){
-//     var tappedViewText = "Tapped index: " + args.index;
-//     pageData.set("tappedViewText", tappedViewText);
+//     var tappedViewText = 'Tapped index: ' + args.index;
+//     pageData.set('tappedViewText', tappedViewText);
 // }
 // var indicatorEnabled = true;
 // exports.toggleIndicator = function(args){
@@ -35,6 +35,6 @@ exports.pageLoaded = function(args) {
 // }
 // exports.toggleColor = function(args){
 //     if(!myCarousel) return;
-//     myCarousel.indicatorColor = "#00B8EB";
-//     myCarousel.indicatorColorUnselected = "#50FED700";
+//     myCarousel.indicatorColor = '#00B8EB';
+//     myCarousel.indicatorColorUnselected = '#50FED700';
 // }
