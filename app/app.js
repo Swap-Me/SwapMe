f3945b43d35bd88a747ff3d2f1ef814e3408e30d
Donald Disha <dondished@gmail.com>
2017-09-27 20:57:43 -0400

    adding linters to project

--- app/app.js
+++ app/app.js
@@ -4,10 +4,10 @@ You can use this file to perform app-level initialization, but the primary
 purpose of the file is to pass control to the app’s first module.
 */
 
-require("./bundle-config");
-var application = require("application");
+require('./bundle-config');
+var application = require('application');
 
-application.start({ moduleName: "main-page" });
+application.start({ moduleName: 'main-page' });
 
 /*
 Do not place any code after the application has been started as it will not
