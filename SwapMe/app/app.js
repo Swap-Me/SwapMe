7ae81c535b3e3d1921769487d63513f046f000a6
codetosavetheworld <31976741+codetosavetheworld@users.noreply.github.com>
2017-11-09 23:27:50 -0500

    Update app.js

--- SwapMe/app/app.js
+++ SwapMe/app/app.js
@@ -3,8 +3,8 @@ var application = require('application');
 var tnsOAuthModule = require("nativescript-oauth");
 
 var facebookInitOptions = {
-    clientId: '137605640319371',
-    clientSecret: 'e627593087ee344069318adc860b56ab',
+    clientId: '',
+    clientSecret: '',
     scope: ['email'] //whatever other scopes you need
 };
 
